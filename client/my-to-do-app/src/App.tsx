3cd59242da5d5d0095f905e84c3e00ddb345c87d
Atanas Zlatev <atanas.zlatev.1998@gmail.com>
2025-10-28 22:00:57 +0200

    styling + active page

--- client/my-to-do-app/src/App.tsx
+++ client/my-to-do-app/src/App.tsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router";
 import "./App.css";
 import Layout from "./components/layout/Layout";
 import Tasks from "./components/tasks/Tasks";
+import Upcoming from "./components/tasks/upcoming";
+import Completed from "./components/tasks/Completed";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Layout>
           <Routes>
             <Route path="/tasks" element={<Tasks />}></Route>
+            <Route path="/upcoming" element={<Upcoming/>}></Route>
+            <Route path="/completed" element={<Completed/>}></Route>
           </Routes>
         </Layout>
       </div>
--- client/my-to-do-app/src/components/menu/MenuBar.tsx
+++ client/my-to-do-app/src/components/menu/MenuBar.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Button } from "../ui/button";
 import { Menu } from "lucide-react";
 import './MenuBar.style.scss';
+import MenuContent from "./MenuContent";
 
 const MenuBar = () => {
   const [activeMenu, setActiveMenu] = useState("w-15");
@@ -15,14 +16,14 @@ const MenuBar = () => {
   };
 
   return (
-    <div className={`${activeMenu} h-screen bg-gray-100 flex flex-col ${activeMenu === 'w-15' ? 'items-center': ''}`}>
+    <div className={`${activeMenu} h-screen bg-gray-light flex flex-col ${activeMenu === 'w-15' ? 'items-center': ''}`}>
       <div className={`flex flex-row justify-between p-2 ${activeMenu === 'w-80' ? 'ps-4 pe-4' : ''}items-center`}>
         <p className={`text-xl font-bold ${activeMenu === 'w-15' ? 'hidden': ''}`}>Menu</p>
         <Button onClick={() => toggleMenu()}>
           <Menu />
         </Button>
       </div>
-      {activeMenu === "w-80" ? <div>CONTENT</div> : <div></div>}
+      {activeMenu === "w-80" ? <MenuContent/> : <div></div>}
     </div>
   );
 };
